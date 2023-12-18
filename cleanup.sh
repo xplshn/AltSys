6a9d25065767718c1dfe1b9188b7c8bcb5ed219f
xplshn <114888778+xplshn@users.noreply.github.com>
2023-12-18 07:35:24 +0000

    Add files via upload

--- cleanup.sh
+++ cleanup.sh
@@ -3,28 +3,32 @@
 # Define directories
 toybox_dir="/opt/AltSys/toybox"
 sbase_dir="/opt/AltSys/obase/sbase"
+ubase_dir="/opt/AltSys/obase/ubase"
 
-# Compare files and remove duplicates
-for file in "$toybox_dir"/bin/*; do
-    file_name=$(basename "$file")
-    if [ -f "$sbase_dir"/bin/"$file_name" ]; then
-        printf "Removing %s from toybox...\n" "$file_name"
-        rm "$file"
-    fi
-done
+# Function to remove duplicate files
+remove_duplicate() {
+    local src_dir="$1"
+    local dest_dir="$2"
 
-for file in "$toybox_dir"/usr/bin/*; do
-    file_name=$(basename "$file")
-    if [ -f "$sbase_dir"/bin/"$file_name" ]; then
-        printf "Removing %s from toybox...\n" "$file_name"
-        rm "$file"
-    fi
-done
+    for file in "$src_dir"/*; do
+        file_name=$(basename "$file")
+        if [ -f "$dest_dir"/bin/"$file_name" ]; then
+            printf "Removing %s from %s...\n" "$file_name" "$src_dir"
+            rm "$file"
+        fi
+    done
+}
 
-for file in "$toybox_dir"/usr/sbin/*; do
-    file_name=$(basename "$file")
-    if [ -f "$sbase_dir"/bin/"$file_name" ]; then
-        printf "Removing %s from toybox...\n" "$file_name"
-        rm "$file"
-    fi
-done
+# Remove duplicates from toybox and ubase
+remove_duplicate "$toybox_dir"/bin "$sbase_dir"
+remove_duplicate "$toybox_dir"/usr/bin "$sbase_dir"
+remove_duplicate "$toybox_dir"/usr/sbin "$sbase_dir"
+
+remove_duplicate "$toybox_dir"/bin "$ubase_dir"
+remove_duplicate "$toybox_dir"/usr/bin "$ubase_dir"
+remove_duplicate "$toybox_dir"/usr/sbin "$ubase_dir"
+
+# Remove duplicate man pages in toybox's share directory if it exists
+if [ -d "$toybox_dir"/share/man ]; then
+    find "$toybox_dir"/share/man -type f -exec sh -c 'file=$(basename "$1"); if [ -f "$ubase_dir/share/man/$file" ]; then rm "$1"; fi' _ {} \;
+fi
