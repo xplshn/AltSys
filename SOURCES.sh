95f1f2385850f52b712a0c272abcdc71fa1bf198
xplshn <114888778+xplshn@users.noreply.github.com>
2023-12-18 04:48:58 -0300

    Update SOURCES.sh

--- SOURCES.sh
+++ SOURCES.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/ksh
 
 # ANSI color codes
 RED='\033[0;31m'
@@ -9,7 +9,7 @@ CYAN='\033[0;36m'
 MAGENTA='\033[0;35m'
 NC='\033[0m' # No Color
 
-# Printing the file content with color codes using printf
+# Updated content reflecting changes from SOURCES file
 printf '%b' "${RED}[anto@beef ~/Documents]@ cat dev/AltSys/opt/SOURCES${NC}\n"
 printf '%b' "${GREEN}core:${NC} doas                            : ${CYAN}https://github.com/slicer69/doas${NC}        |${BLUE}commit: e9bee5f17210a97a40c6cdc130740fa2d70f9b0e${NC}\n"
 printf '%b' "${YELLOW}misc:${NC} fortune                         : ${CYAN}https://github.com/cafkafk/fortune-kind${NC} |${BLUE}commit: 120798bf41c5b0b28fdd9025bf8b1ebe2d2193c7${NC}\n"
@@ -20,6 +20,5 @@ printf '%b' "${YELLOW}misc:${NC} ccat, ttts                      : ${CYAN}https:
 printf '%b' "${GREEN}coreutils:${NC} Toybox: ${CYAN}https://github.com/landley/toybox${NC}                            |${BLUE}commit: ab046139f9d83136ff1cb143ee5923b3ee22a972${NC}\n"
 printf '%b' "${GREEN}coreutils:${NC} Ubase : ${CYAN}https://git.suckless.org/ubase/${NC}                       |${MAGENTA}commit-date: 2023-09-22 08:06${NC}\n"
 printf '%b' "${GREEN}coreutils:${NC} Sbase : ${CYAN}https://git.suckless.org/sbase/${NC}                       |${MAGENTA}commit-date: 2023-12-01 12:33${NC}\n"
-
-printf '%b' "${RED}______________________________________2023-12-15${NC}\n"
+printf '%b' "${RED}________________________________________2023-12-15${NC}\n"
 printf '%b' "${YELLOW}Info:${NC} The coreutils are used in this order: if \$COMMAND is provided by ubase, but also by sbase, and also by toybox, the system will use Toybox's, if the command is provided only by sbase, or ubase, the system will use that, same goes for toybox, if the command is not found on /opt/AltSys/*/*, the system will fallback to the commands on /bin, /usr/bin, etc. See /etc/profile\n"
